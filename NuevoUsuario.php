fed21d8cef93a10809367e4a0a3954f7c20443de
SoniaSegovia <segoviasonia267@gmail.com>
2022-05-13 08:47:24 -0600

    seguir

--- NuevoUsuario.php
+++ NuevoUsuario.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+include ('Config.php');
+if (!isset($_SESSION['user_id'])) {
+    header('Location: Index.php');
+    exit;
+}
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -7,86 +16,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Nuevo Usuario</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <?php require_once "Menu.php" ?>
+
+    <script src="https://kit.fontawesome.com/de32201dc5.js" crossorigin="anonymous"></script>
+
+    <title>Lista de Mascotas</title>
 </head>
 
 <body>
 
-    <div class="container"><br>
-        <div class="row justify-content-center">
-            <div class="col-6 p-5 bg-white shadow-lg rounded">
-                <h3>Nuevo Usuario</h3>
-                <hr>
+    <div class="container">
+        <table class="table" id="mascotas">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nombre</th>
+                    <th>Tipo de Animal</th>
+                    <th>Raza</th>
+                    <th>Color</th>
+                    <th>Sexo</th>
+                    <th>Edad</th>
+                    <th>Peso</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
                 <?php
-                include('Config.php');
-                session_start();
-
-                if (isset($_POST['AgregarUsuario'])) 
-                {
-                    $username = $_POST['usuario'];
-                    $email = $_POST['email'];
-                    $password = $_POST['password'];
-                    $password_hash = password_hash($password, PASSWORD_BCRYPT);
-                    $query = $conn->prepare("SELECT * FROM usuarios WHERE email=:email");
-                    $query->bindParam("email", $email, PDO::PARAM_STR);
-                    $query->execute();
-
-                    if ($query->rowCount() > 0) 
-                    {
-                        echo '<div class="alert alert-danger" role="alert">
-                                    ¡La dirección de correo electrónico ya está registrada!
-                              </div>';
-                    }
-
-                    if ($query->rowCount() == 0) {
-                        $query = $conn->prepare("INSERT INTO usuarios (Username, Password, Email) VALUES (:username,:password_hash,:email)");
-                        $query->bindParam("username", $username, PDO::PARAM_STR);
-                        $query->bindParam("password_hash", $password_hash, PDO::PARAM_STR);
-                        $query->bindParam("email", $email, PDO::PARAM_STR);
-                        $result = $query->execute();
-
-                        if ($result) 
-                        {
-                            echo '<div class="alert alert-success" role="alert">Tu registro fue exitoso!</div>';
-                        } 
-                        else 
-                        {
-                            echo '<div class="alert alert-danger" role="alert">¡Algo salió mal!</div>';
-                        }
-                    }
-                }
+                $consulta = "SELECT * FROM mascotas";
+                $llamado -> Listar($consulta);
                 ?>
-
-                <form method="post">
-                    <div class="form-group">
-                        <label for="usuario">Nombre del Usuario</label>
-                        <input id="usuario" class="form-control" type="text" name="usuario">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input id="email" class="form-control" type="email" name="email">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input id="password" class="form-control" type="password" name="password">
-                    </div>
-
-                    <br>
-
-                    <div class="d-grid gap-2 d-md-block text-center">
-                        <a class="btn btn-secondary col-5" href="Index.php">Cancelar</a>
-                        <button class="btn btn-primary col-5" name="AgregarUsuario" type="submit">Registrarse</button>
-                    </div>
-                </form>
-            </div>
-        </div>
+            </tbody>
+        </table>
     </div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
